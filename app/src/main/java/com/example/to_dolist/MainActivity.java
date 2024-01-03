e2aab7e332dc631ba13496e69ef44e275811a0f2
dulanthaM <76222136+dulanthaM@users.noreply.github.com>
2024-01-04 03:59:05 +0530

    completed

--- app/src/main/java/com/example/to_dolist/MainActivity.java
+++ app/src/main/java/com/example/to_dolist/MainActivity.java
@@ -15,16 +15,22 @@ import android.widget.Button;
 
 import com.example.to_dolist.Adapter.ToDoAdapter;
 import com.example.to_dolist.Model.ToDoModel;
+import com.example.to_dolist.Utils.DatabaseHandler;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DialogCloseListener{
 
     private RecyclerView tasksRescycleView;
     private ToDoAdapter tasksAdapter;
 
     private List<ToDoModel> taskList;
+    private DatabaseHandler db;
+    private FloatingActionButton fab;
 
 
     @Override
@@ -33,13 +39,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
+        db = new DatabaseHandler(this);
+        db.openDatabase();
+
         taskList = new ArrayList<>();
 
         tasksRescycleView = findViewById(R.id.taskRecyclerviviw);
         tasksRescycleView.setLayoutManager(new LinearLayoutManager(this));
-        tasksAdapter = new ToDoAdapter(this);
+        tasksAdapter = new ToDoAdapter(db,MainActivity.this);
         tasksRescycleView.setAdapter(tasksAdapter);
 
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new RecyclerItemTouchHelper(tasksAdapter));
+        itemTouchHelper.attachToRecyclerView(tasksRescycleView);
+
+        /* dummy value
         ToDoModel task = new ToDoModel();
         task.setTask("this is a test task");
         task.setStatus(0);
@@ -49,7 +62,31 @@ public class MainActivity extends AppCompatActivity {
         taskList.add(task);
         taskList.add(task);
         taskList.add(task);
+        */
+        /*ItemTouchHelper itemTouchHelper = new
+                ItemTouchHelper(new RecyclerItemTouchHelper(tasksAdapter));
+        itemTouchHelper.attachToRecyclerView(tasksRecyclerView);*/
+
+        fab = findViewById(R.id.fab);
+
+        taskList = db.getAllTasks();
+        Collections.reverse(taskList);
 
         tasksAdapter.setTasks(taskList);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddNewTask.newInstance().show(getSupportFragmentManager(), AddNewTask.TAG);
+            }
+        });
+    }
+
+    @Override
+    public void handleDialogClose(DialogInterface dialog){
+        taskList = db.getAllTasks();
+        Collections.reverse(taskList);
+        tasksAdapter.setTasks(taskList);
+        tasksAdapter.notifyDataSetChanged();
     }
 }
